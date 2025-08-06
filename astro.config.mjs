04b09c9ff1b499095799ee1d1f36721d6c872c30
dw.office <dimaswahyu.official@gmail.com>
2025-08-06 10:11:02 +0700

    add to builder

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     site: 'https://dimaswahyu-official.github.io',
     base: '/',
+    outDir: 'docs', 
     output: 'static',
     trailingSlash: 'always',
 
